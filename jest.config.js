8ae455ab2e4ea7ffc14144a86ffae8405fb4c33b
jotavec <jotave.face@gmail.com>
2025-08-15 20:49:00 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,32 @@
+/** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
+  // Encontre testes em __tests__ e arquivos *.test.js
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+
+  // Transpile com Babel (Node atual)
   transform: {
-    '^.+\\.js$': 'babel-jest'     // se for JS puro, ou
-    // '^.+\\.ts$': 'ts-jest'      // se você usar TS
+    '^.+\\.[jt]sx?$': 'babel-jest',
   },
-  testMatch: ['**/__tests__/**/*.test.js']
-}
+
+  // Deixe o Jest mais previsível
+  clearMocks: true,
+  restoreMocks: true,
+
+  // Ignorar pastas geradas
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/build/', '<rootDir>/coverage/'],
+
+  // Cobertura
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+
+  // Arquivos de setup (opcional)
+  // setupFiles: ['<rootDir>/.jest/env.js'],
+  // setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
+
+  // Tempo padrão p/ testes mais lentos (ex.: integrações)
+  testTimeout: 10000,
+
+  verbose: true,
+};
